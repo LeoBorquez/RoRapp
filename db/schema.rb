c5aac4bbacd8fa2a41220aecaecee4842cbce581
Leo Borquez <ele.dev@protonmail.com>
2018-12-27 18:22:54 -0500

    Index user
    Index added to user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_08_200639) do
+ActiveRecord::Schema.define(version: 2018_12_27_230614) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name_category"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2018_10_08_200639) do
     t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["mail"], name: "index_users_on_mail", unique: true
   end
 
 end
