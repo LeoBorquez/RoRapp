c30734a22480aad2ffb99dbeac972a1b6e5f3009
Leo Borquez <ele.dev@protonmail.com>
2019-01-03 21:31:15 -0500

    Hash password User
    New column in user model for hashed passwords

--- Gemfile
+++ Gemfile
@@ -68,3 +68,5 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'font-awesome-rails', '4.7.0.4'
 gem 'jquery-rails', '4.3.3'
 gem 'bootstrap', '4.1.3'
+
+gem 'bcrypt', '3.1.12'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_27_230614) do
+ActiveRecord::Schema.define(version: 2019_01_04_020403) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name_category"
@@ -39,11 +39,12 @@ ActiveRecord::Schema.define(version: 2018_12_27_230614) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "mail"
+    t.string "email"
     t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["mail"], name: "index_users_on_mail", unique: true
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
