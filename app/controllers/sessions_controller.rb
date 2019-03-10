0c7c323ae0485473079225538564b5ed11ff6ad4
Leo Borquez <ele.dev@protonmail.com>
2019-03-10 18:21:35 -0400

    Secure session
    Secure session cookie

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,12 +10,12 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase) #Get email user from database
     if user && user.authenticate(params[:session][:password]) #Get password from database
       log_in user #Method declared in sessions_helper.rb
+      remember user
       redirect_to user #Rails automatically converts this to the route for the user’s profile page:
     else
       flash.now[:danger] = 'Invalid email/password combination' #Disappear after additional request
       render 'new'
     end
-
 
   end
 
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,6 +5,14 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  # Remember a user in a persistent session
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  # logs out the current user
   def log_out
     session.delete(:user_id)
     @current_user = nil
