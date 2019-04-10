155d6afbadcb13f380ad53b4d65e325809804bca
Leo Borquez <ele.dev@protonmail.com>
2019-04-09 23:55:58 -0400

    Redesign flow
    New flow on the main idea

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,8 +26,10 @@ class UsersController < ApplicationController
     else
       render 'users/new'
     end
+  end
 
-
+  def edit
+    @user = User.find(params[:id])
   end
 
   private def user_params
