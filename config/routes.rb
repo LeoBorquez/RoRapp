6a16c3570cd0372baa284c354f513bb6663fb22f
Leo Borquez <ele.dev@protonmail.com>
2019-05-23 18:01:54 -0400

    Account Activation
    New model to account activation and routes was modify.

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,21 @@
 Rails.application.routes.draw do
   get 'sessions/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  #
-  get 'pages/index'
-  get 'products/index'  #GET because is a get request
-  get 'users/index'
+
 
   get 'contact', to: 'pages#contact' #admin layout, pages only for admin
-  get 'help', to: 'pages#help'
+  get 'help', to: 'pages#help' #GET because is a get request
 
   get 'signup', to: 'users#new'
-  post 'signup', to: 'users#create'
 
   get 'login', to: 'sessions#new'
-  get 'logout', to: 'sessions#destroy'
   post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 
 
-
-
-  resources :products
-  resources :categorys
   resources :users
   resources :sessions
+  resources :account_activations, only: [:edit]
 
   root 'pages#index'
 
