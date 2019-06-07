c42023580c20ff94a417a7a80165b99c4de8fe33
Leo Borquez <ele.dev@protonmail.com>
2019-06-07 17:55:03 -0400

    Mail test

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -1,9 +1,12 @@
 require 'test_helper'
 
 class UserMailerTest < ActionMailer::TestCase
+
   test "account_activation" do
-    mail = UserMailer.account_activation
+    user = users(:one) user.activation_token = User.new_token
+    mail = UserMailer.account_activation(user)
     assert_equal "Account activation", mail.subject
+    assert_equal [user.email], mail.to
     assert_equal ["to@example.org"], mail.to
     assert_equal ["from@example.com"], mail.from
     assert_match "Hi", mail.body.encoded
