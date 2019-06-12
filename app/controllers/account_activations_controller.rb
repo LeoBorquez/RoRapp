8d8ca28b337a978cd7670b3fe6dfabebcaf3843d
Leo Borquez <ele.dev@protonmail.com>
2019-06-11 21:22:42 -0400

    User activated?
    Validation if user is activated and authenticated

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,2 +1,20 @@
 class AccountActivationsController < ApplicationController
+
+  def edit
+    user = User.find_by(email: params[:email])
+
+    #user dont activated && user authenticated
+    if user && !user.activated? && user.authenticated?(:activation, params[:id])
+      user.update_attribute(:activated, true)
+      user.update_attribute(:activated_at, Time.zone.now)
+      log_in user
+      flash[:success] = "Account Activated!"
+      redirect_to user
+    else
+      flash[:danger] = "Invalid activation link"
+      redirect_to root_url
+    end
+
+  end
+
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,10 +8,18 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase) #Get email user from database
     if user && user.authenticate(params[:session][:password]) #Get password from database
-      log_in user #Method declared in sessions_helper.rb
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user) #[] override param arguments
-      redirect_back_or user
-      #redirect_to user #Rails automatically converts this to the route for the user’s profile page
+      if user.activated?
+        log_in user #Method declared in sessions_helper.rb
+        params[:session][:remember_me] == '1' ? remember(user) : forget(user) #[] override param arguments
+        redirect_back_or user
+        #redirect_to user #Rails automatically converts this to the route for the user’s profile page
+      else
+        message = "Account not activated. "
+        message += "Check your email for the activation link."
+        flash[:warning] = message
+        redirect_to root_url
+      end
+
     else
       flash.now[:danger] = 'Invalid email/password combination' #Disappear after additional request
       render 'new'
@@ -21,7 +29,7 @@ class SessionsController < ApplicationController
 
   def destroy
     log_out if logged_in?
-    redirect_to root_url # Change to admin root
+    redirect_to root_url # Change to root
   end
 
 end
