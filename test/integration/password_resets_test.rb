3ce8ec7a9483ea1684d4e1e678436a4e4faa1bda
Leo Borquez <ele.dev@protonmail.com>
2019-06-16 12:28:28 -0400

    Password reset test

--- test/integration/password_resets_test.rb
+++ test/integration/password_resets_test.rb
@@ -15,17 +15,58 @@ class PasswordResetsTest < ActionDispatch::IntegrationTest
     assert_template ' password_reset/new'
 
     # Invalid email
-    post password_reset_path, params: { passwor_reset: { email: "" } }
+    post password_reset_path, params: {passwor_reset: {email: ""}}
     assert_not flash.empty?
     assert_template 'password_reset/new'
 
     # Valid email
     post password_reset_path,
-         params: { password_reset: { email: @user.email } }
+         params: {password_reset: {email: @user.email}}
     assert_not_equal @user.reset_digest, @user.reload.reset_digest
     assert_equal 1, ActionMailer::Base.deliveries.size
     assert_not flash.empty?
     assert_redirect_to root_url
+
+    # Password reset form
+    user = assigns(:user)
+
+    # Wrong email
+    get edit_password_reset_path(user.reset_token, email: "")
+    assert_redirected_to root_url
+
+    # Inactive user
+    user.toggle!(:activated)
+    get edit_password_reset_path(user.reset_token, email: user.email)
+    assert_redirected_to root_url
+    user.toggle!(:activated)
+
+    # Right email, wrong token
+    get edit_password_reset_path('wrong token', email: user.email)
+    assert_redirected_to root_url
+
+    # Right email, right token
+    get edit_password_reset_path(user.reset_token, email: user.email)
+    assert_template 'password_resets/edit'
+    assert_select "input[name=email][type=hiddden][value=?]", user.email
+
+    # Invalid password & confirmation
+    patch password_reset_path(user.reset_token),
+          params: {email: user.email,
+                   user: {password: "foobaz",
+                          password_confirmation: "asdadad"
+                   }}
+    assert_select 'div#error_explanation'
+
+    # Empty password
+    patch password_reset_path(user.reset_token),
+          params: { email: user.email,
+                    user: { password: "",
+                            password_confimartion: ""
+
+                    }}
+    assert_select 'div#error_explanation'
+
+    
 
   end
 
