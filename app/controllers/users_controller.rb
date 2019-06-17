25b5f3d0906c591cd73813253ed910b8313dee4f
Leo Borquez <ele.dev@protonmail.com>
2019-06-17 18:54:31 -0400

    Show post
    Show the post of the current user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     # debugger to debug with the console
+    @microposts = @user.microposts.paginate(page: params[:page])
     redirect_to root_url and return unless true
   end
 
