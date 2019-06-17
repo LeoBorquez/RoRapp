c42435fdf05ffcc4e15ed9326d3a8eaf3bba6b6b
Leo Borquez <ele.dev@protonmail.com>
2019-06-17 17:56:23 -0400

    Post scope
    Recent post first

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,8 @@
 class Micropost < ApplicationRecord
   belongs_to :user
+
+  default_scope -> { order(created_at: :desc) } # Show the recent post first
+
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
 end
