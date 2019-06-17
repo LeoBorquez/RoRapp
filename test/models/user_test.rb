a6b9a4b0fbac3812fcb3a323d731d0b1109b36f7
Leo Borquez <ele.dev@protonmail.com>
2019-06-17 18:11:29 -0400

    Unit test destroy
    Destroy post with the user

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -13,4 +13,12 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.authenticated?(:remember, '')
   end
 
+  test "associated microposts should be destroyed" do
+    @user.save
+    @user.microposts.create!(content: "test test")
+    assert_difference 'Micropost.count', -1 do
+      @user.destroy
+    end
+  end
+
 end
