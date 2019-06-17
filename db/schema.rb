4459758bdafd915c3eb9ae19104fd4fa0d3b92ca
Leo Borquez <ele.dev@protonmail.com>
2019-06-17 11:22:20 -0400

    Post model
    New post model to create post?

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_15_023811) do
+ActiveRecord::Schema.define(version: 2019_06_17_150208) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name_category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
+    t.index ["user_id"], name: "index_microposts_on_user_id"
   end
 
   create_table "product_comments", force: :cascade do |t|
---
+++ test/models/micropost_test.rb
@@ -0,0 +1,12 @@
+require 'test_helper'
+
+class MicropostTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+
+  def setup
+    @user = users(:one)
+  end
+
+end
