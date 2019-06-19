18b4ab604d72c498299079bccb21f6f8d0f01025
Leo Borquez <ele.dev@protonmail.com>
2019-06-19 18:44:06 -0400

    Refactor
    the logged_in_user was moved to the application controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper #including the module into the base class of all controllers
+
+  # Confirms a logged in user
+  private def logged_in_user
+    unless logged_in?
+      store_location # Store the actual location of the user.
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
+
 end
