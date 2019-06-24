7821889aea66e3b5f409ded0be5163d63cfc0e2b
Leo Borquez <ele.dev@protonmail.com>
2019-06-24 18:03:37 -0400

    Delete post

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,6 @@
 class MicropostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
+  before_action :correct_user, only: :destroy
 
 begin
   def new
@@ -19,11 +20,18 @@ end
   end
 
   def destroy
-
+    @micropost.destroy
+    flash[:success] = "Post deleted"
+    redirect_to request.referrer || root_url # Request the previous URl
   end
 
   private def micropost_params
     params.require(:micropost).permit(:content)
+  end
+
+  def correct_user
+    @micropost = current_user.microposts.find_by(id: params[:id])
+    redirect_to root_url if @microposts.nil?
   end
 
 end
