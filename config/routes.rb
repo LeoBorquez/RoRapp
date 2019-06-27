91b9578d2170b3f23b346e1259ca14785534fd80
Leo Borquez <ele.dev@protonmail.com>
2019-06-27 17:55:30 -0400

    Display stats

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,12 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers # /users/1/following and /users/1/followers
+    end
+  end
+
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
   resources :microposts, only: [:create, :destroy]
