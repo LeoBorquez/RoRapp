a2bafb3b594ad52d2e214d95ad5ace7ca5397f04
Leo Borquez <ele.dev@protonmail.com>
2019-07-01 21:13:29 -0400

    JS files

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,10 @@ module ECommerce
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+
+    # Include authenticity token in remote farms
+    config.action_view.embed_authenticity_token_in_remote_forms = true
+
   end
 end
